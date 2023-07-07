5063c99b7ff87a19f375856e9ed25b3e2ca87f96
Artur-man <123097639+arthyr-men@users.noreply.github.com>
2023-07-07 17:52:07 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,23 @@
-// Отримуємо елемент ul#categories
-const categoriesList = document.querySelector('#categories');
+document.addEventListener('DOMContentLoaded', () => {
+  // Отримуємо елемент ul#categories
+  const categoriesList = document.querySelector('#categories');
 
-// Отримуємо всі елементи li.item у списку ul#categories
-const categoryItems = categoriesList.querySelectorAll('li.item');
+  // Отримуємо всі елементи li.item у списку ul#categories
+  const categoryItems = categoriesList.querySelectorAll('li.item');
 
-// Виводимо кількість категорій
-console.log('Number of categories:', categoryItems.length);
+  // Виводимо кількість категорій
+  console.log('Number of categories:', categoryItems.length);
 
-// Перебираємо кожен елемент li.item
-categoryItems.forEach((item) => {
-  // Отримуємо заголовок (текст елемента <h2>)
-  const categoryTitle = item.querySelector('h2').textContent;
+  // Перебираємо кожен елемент li.item
+  categoryItems.forEach((item) => {
+    // Отримуємо заголовок (текст елемента <h2>)
+    const categoryTitle = item.querySelector('h2').textContent;
 
-  // Отримуємо всі елементи <li> у категорії
-  const categoryElements = item.querySelectorAll('li');
+    // Отримуємо всі елементи <li> у категорії
+    const categoryElements = item.querySelectorAll('li');
 
-  // Виводимо назву категорії і кількість елементів у ній
-  console.log('Category:', categoryTitle);
-  console.log('Elements:', categoryElements.length);
+    // Виводимо назву категорії і кількість елементів у ній
+    console.log('Category:', categoryTitle);
+    console.log('Elements:', categoryElements.length);
+  });
 });
