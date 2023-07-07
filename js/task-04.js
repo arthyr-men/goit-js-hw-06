ff09b6a5394943dd4b8886c6557533490c97c624
Artur-man <123097639+arthyr-men@users.noreply.github.com>
2023-07-07 17:46:46 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,11 @@
-const counterValue = 0;
+let counterValue = 0;
 const valueEl = document.getElementById("value");
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 
 function updateCounter(value) {
   valueEl.textContent = value;
+  counterValue = value; // Оновити значення лічильника
 }
 
 incrementBtn.addEventListener("click", () => {
@@ -19,3 +20,4 @@ decrementBtn.addEventListener("click", () => {
 
 // Оновити значення лічильника при завантаженні сторінки
 updateCounter(counterValue);
+
